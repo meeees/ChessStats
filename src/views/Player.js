174b64c2178fdf293fb513d06c2b894a3c8157a7
Michael <mcbarre12@gmail.com>
2021-01-03 22:00:59 -0600

    basic pulling api data

--- src/views/Player.js
+++ src/views/Player.js
@@ -1,11 +1,86 @@
-import React from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
+import ChessWebAPI from 'chess-web-api';
 import PropTypes from 'prop-types';
 
-function Player(props) {
-  return <div>{props.name} is not implemented yet :(</div>;
+
+function Player() {
+  const [pName, setPName] = useState('meeees');
+  const [playerData, setPlayerData] = useState({});
+  const [playerFrags, setPlayerFrags] = useState([]);
+  const [playerStats, setPlayerStats] = useState([]);
+  useEffect(() => {
+    console.log(playerData);
+    const tmp = [];
+    for (const pd in playerData.base) {
+      tmp.push(<Fragment key={pd}>{pd}: {playerData.base[pd]}<br/></Fragment>);
+    }
+    const tmp2 = [];
+    for(const pd in playerData.stats) {
+      if(typeof(playerData.stats[pd]) === 'object') {
+        if(pd.startsWith('chess')) {
+          tmp2.push(<ChessSection key={pd} name={pd.substr(6)} record={playerData.stats[pd].record}/>);
+        }
+        else {
+          tmp2.push(<Fragment key={pd}>{pd}: object<br/></Fragment>);
+        }
+      }
+      else {
+        tmp2.push(<Fragment key={pd}>{pd}: {playerData.stats[pd]}<br/></Fragment>);
+      }
+    }
+    setPlayerFrags(tmp);
+    setPlayerStats(tmp2);
+
+  }, [playerData]);
+
+  
+  function requestPlayerData(name) {
+    console.log(name);
+    var api = new ChessWebAPI();
+    const tmp = {};
+    api.getPlayer(name).then(r => {tmp.base = r.body;})
+      .then(() => api.getPlayerStats(name)
+        .then(r => {tmp.stats = r.body;}))
+      .then(() => setPlayerData(tmp));
+    
+
+  }
+  
+  return <div>
+    <form onSubmit={(e) => {
+      requestPlayerData(pName);
+      e.preventDefault();
+    }}>
+      <input type='text' value={pName} onChange={e =>{setPName(e.target.value);}}/>
+      <button>Submit</button>
+    </form>
+    {playerFrags}
+    <br/>
+    {playerStats}
+    <br/>
+  </div>;
 }
 
-Player.propTypes = {
+function percentify(f, p) {
+  return (f * 100).toFixed(p);
+}
+
+function ChessSection(props) {
+  console.log(props.record);
+  const {win, loss, draw} = props.record;
+  var total = win + loss + draw;
+  total = total > 0 ? total : 1;
+  return <div>
+    <b>{props.name}</b>
+    <br/>
+    W/L/D {win}/{loss}/{draw} {percentify(win/total)}/{percentify(loss/total)}/{percentify(draw/total)}
+    <br/>
+    <br/>
+  </div>;
+}
+
+ChessSection.propTypes = {
+  record: PropTypes.object,
   name: PropTypes.string
 };
 
